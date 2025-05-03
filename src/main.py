5763ed396292c94e73c78fda8f962c9aeb6ee802
Robin524970 <robinpellis@protonmail.com>
2025-05-03 19:18:03 +0100

    added extract markdown images and links

--- src/main.py
+++ src/main.py
@@ -3,9 +3,17 @@ from textnode import (
 	TextNode,
 )
 
+from inline_markdown import (
+	split_nodes_delimiter,
+	extract_markdown_images,
+	extract_markdown_links,
+)
+
 def main():
-	text_node = TextNode("Bla Bla", TextType.LINK, "http://www.boot.dev")
-	text_node2 = TextNode("Bla Bla bla", TextType.BOLD, "http://www.boot.dev")
-	print(text_node)
+	text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+	print(extract_markdown_images(text))
+
+	text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+	print(extract_markdown_links(text))
 
 main()
